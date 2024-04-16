4b18606d6e026fb864a1e6814eeedfd90eaf8dd9
Prashanth <Tamilanth@protonmail.com>
2024-04-16 10:18:21 +0300

    Add dataon different levels

--- assets/script/workOut.py
+++ assets/script/workOut.py
@@ -1,11 +1,11 @@
 import csv
 
-filename = "beginner.js"
+filename = "expert.js"
 with open(filename, 'w') as textfile:
     textfile.write('const beginner = [')
     with open('workOut.csv', newline='') as csvfile:
           spamreader = csv.reader(csvfile, delimiter=',', quotechar='|')
           for x in spamreader:
-              if x[2] == "Beginner":
+              if x[2] == "Expert":
                   textfile.write(f'"{x[1]}",')
     textfile.write('];')
